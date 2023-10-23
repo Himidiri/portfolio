01e683950516c32b11dd50c9d40d7f861f545bc9
Himidiri <himidirihimakanika@gmail.com>
2023-10-23 18:24:08 +0530

    Added Education Section

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Container, Wrapper, Title, Description, TimelineSection } from './EducationStyle';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { education } from '../../data/constants';
+import EducationCard from '../Cards/EducationCard';
 
-const Education = () => {
+const index = () => {
   return (
-    <div>Education</div>
+    <Container id="education">
+      <Wrapper>
+        <Title>Education</Title>
+        <Description>
+          Education is my foundation. Explore my academic journey to uncover the wealth of knowledge and skills I've gained. Here, you'll find the details of my achievements.
+        </Description>
+        <TimelineSection>
+          <Timeline>
+            {education.map((education, index) => (
+              <TimelineItem >
+                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                  <EducationCard education={education} />
+                </TimelineContent>
+                <TimelineSeparator>
+                  <TimelineDot variant="outlined" color="secondary" />
+                  {index !== education.length && <TimelineConnector style={{ background: '#854CE6' }} />}
+                </TimelineSeparator>
+              </TimelineItem>
+            ))}
+          </Timeline>
+
+        </TimelineSection>
+      </Wrapper>
+    </Container>
   )
 }
 
-export default Education
+export default index
